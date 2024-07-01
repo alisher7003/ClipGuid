a2cd76813c334eed4b6eba754e33c1ae53c41514
alisher7003 <alisher.ishniyozov@gmail.com>
2024-07-01 21:38:46 +0500

    Add DateTimeOffSet Guid

--- ClipGuid/Program.cs
+++ ClipGuid/Program.cs
@@ -1,12 +1,23 @@
 ﻿string selection = "";
 do
 {
-    string stringFieldGuid = Guid.NewGuid().ToString();
-    Console.WriteLine(stringFieldGuid);
-    TextCopy.ClipboardService.SetText(stringFieldGuid);
+    Console.WriteLine("1. NewGuid\n2. DateTimeOffset");
+    int input = int.Parse(Console.ReadLine());
+    if (input == 1)
+    {
+        string stringFieldGuid = Guid.NewGuid().ToString();
+        Console.WriteLine(stringFieldGuid);
+        TextCopy.ClipboardService.SetText(stringFieldGuid);
+    }
+    if (input == 2)
+    {
+        string dateTimeOffSet = DateTimeOffset.Now.ToString();
+        Console.WriteLine(dateTimeOffSet);
+        TextCopy.ClipboardService.SetText(dateTimeOffSet);
+    }
     Console.WriteLine("Copied");
 
-    Console.WriteLine("Do you want again NewGuid? press yes");
+    Console.WriteLine("Do you want again? press yes");
     selection = Console.ReadLine();
 }
 while (selection == "yes");
