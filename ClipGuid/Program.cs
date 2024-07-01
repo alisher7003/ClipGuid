b2419230c17690cee4a1388203c9a9f7fc9f82f9
alisher7003 <alisher.ishniyozov@gmail.com>
2024-07-01 21:18:39 +0500

    Добавьте файлы проекта.

---
+++ ClipGuid/Program.cs
@@ -0,0 +1,12 @@
+﻿string selection = "";
+do
+{
+    string stringFieldGuid = Guid.NewGuid().ToString();
+    Console.WriteLine(stringFieldGuid);
+    TextCopy.ClipboardService.SetText(stringFieldGuid);
+    Console.WriteLine("Copied");
+
+    Console.WriteLine("Do you want again NewGuid? press yes");
+    selection = Console.ReadLine();
+}
+while (selection == "yes");
